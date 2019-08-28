fb4cf993417819c55aa4536736d204ff75b7ac64
Sriram <46366477+ram170@users.noreply.github.com>
2019-08-28 10:12:00 +0530

    Update AL DB.py

--- AL DB.py
+++ AL DB.py
@@ -1,56 +1,38 @@
 import docx
-from PIL import Image
 from docx import Document
-from pytesseract import image_to_string
+import xlrd
 import os
 def read():
-    document=Document()
-    x=os.listdir('/01/Image processing/Images/')
-    s='/01/Image processing/Images/'+str(x[0])
-    img=Image.open(s)
-    text=image_to_string(img)
-    #print(text)
-    document.save('C:\\01\\Image processing\\Chasisno\\'+text)
-    os.remove(s)
-    return text
-x=os.listdir('/01/Image processing/Backup/')
-document= Document('/01/Image processing/Backup/bup.docx')
-for para in document.paragraphs:
-    temp=para.text
-    #print(temp)
-l=list(temp.split(" "))
-#print(l)    
-while(1):
-   print("What do you want to do?")
-   print("1. Including New vehicle")
-   print("2. Map")  
-   print("3. Remove")
-   do=int(input())
-   if(do==1):
-        chasisno=read()
-        print("Enter position")
-        get=int(input())
-        l[get-1]=chasisno
-        text2=str()
-        document2=Document()
-        for i in range(0,len(l)):
-          if(i!=len(l)-1):
-            text2=text2+str(l[i])+" "
-          else:
-            text2=text2+str(l[i])
-        para1=document2.add_paragraph(text2)
-        #print(text2)      
-        document2.save('C:\\01\\Image processing\\Backup\\bup.docx')
-   if(do==2):
-       for i in range(0,len(l)):
-           print(i+1,l[i])
-   if(do==3):
-       print("The position to remove?")
-       r=int(input())
-       print("The chasis no at the given position is",l[r-1])
-       print("Remove? press Y or N")
-       if(input()=='Y'):
-           l[r-1]=0
+    loc = ("C:\\01\\Image processing\\Excel Test\\Book1.xlsx") 
+    rd=[]
+    wb = xlrd.open_workbook(loc) 
+    sheet = wb.sheet_by_index(0) 
+    sheet.cell_value(0, 0) 
+    for i in range(sheet.nrows): 
+    	rd.append(sheet.cell_value(i, 0))
+    return rd	
+def map(l):
+    print(1,l[0],end=" | ")
+    for i in range(1,len(l)):
+        if(i%6!=0 and i!=0):
+             print(i+1,l[i],end=" | ")
+        else:
+            print(i+1,l[i])        
+def remove(l):
+       print("How many vehicles do you have to remove?")
+       No_Veh=int(input())
+       i=0
+       while(i!=No_Veh):
+            print("The position of vehicle",i+1,"to remove?")
+            r=int(input())
+            try: 
+               print("The chasis no at the given position is",l[r-1])
+               print("Remove? press Y or N")
+               if(input()=='Y'):
+                   l[r-1]=0
+               i+=1    
+            except:
+                print("Enter a valid location")
        text2=str()    
        document2=Document()
        for i in range(0,len(l)):
@@ -61,4 +43,93 @@ while(1):
        para1=document2.add_paragraph(text2)
        #print(text2)      
        document2.save('C:\\01\\Image processing\\Backup\\bup.docx')
-
+       return l
+def search(l):
+    print("Enter the number of vehicles you wanna search?")
+    No_Veh=int(input())
+    op=[]
+    for i in range(0,No_Veh):
+        print("Enter Chasis No",i+1) 
+        op.append(input())
+    for i in range(0,No_Veh):
+        if op[i] in l:
+            x=l.index(op[i])
+            print(op[i],"is present in",x+1)
+        else:
+            print(op[i],"is not present")
+def reallocate(l):
+    print("Enter the location from which you wish to move the vehicle")
+    l1=int(input())
+    print("Enter the location to which you want to move the vehicle")
+    l2=int(input())
+    try:
+       temp=l[l2-1]
+       l[l2-1]=l[l1-1]
+       l[l1-1]=temp
+       #print(l)
+       print("Reallocation complete")
+    except:
+        print("Please Enter valid Location")
+    document2=Document()
+    text2=str()
+    for i in range(0,len(l)):
+          if(i!=len(l)-1):
+            text2=text2+str(l[i])+" "
+          else:
+            text2=text2+str(l[i])
+    para1=document2.add_paragraph(text2)
+        #print(text2)      
+    document2.save('C:\\01\\Image processing\\Backup\\bup.docx')
+    return l
+x=os.listdir('/01/Image processing/Backup/')
+document= Document('/01/Image processing/Backup/bup.docx')
+for para in document.paragraphs:
+    temp=para.text
+    #print(temp)
+l=list(temp.split(" "))
+#print(l)    
+while(1):
+   print("What do you want to do?")
+   print("1. Inward New vehicle")
+   print("2. Map")  
+   print("3. Remove")
+   print("4. Search")
+   print("5. Reallocate")
+   do=int(input())
+   if(do==1):
+      rd=[]
+      rd=read()
+      for i in range(0,len(rd)):
+       if rd[i] not in l:   
+        print("Is it a")
+        print("1. Longer Wheel Base Vehicle")
+        print("2. Shorter Wheel Base Vehicle")
+        Type=int(input())
+        if(Type==1):
+            print("Enter position between 1 to 25")
+        elif(Type==2):
+            print("Enter position between 26 to 50")
+        get=int(input())
+        l[get-1]=rd[i]          
+        text2=str()
+        document2=Document()
+        for i in range(0,len(l)):
+          if(i!=len(l)-1):
+            text2=text2+str(l[i])+" "
+          else:
+            text2=text2+str(l[i])
+        para1=document2.add_paragraph(text2)
+        #print(text2)      
+        document2.save('C:\\01\\Image processing\\Backup\\bup.docx')
+        print("Vehicle Inwarded")
+   if(do==2):
+       map(l)
+       print("")    
+   if(do==3):
+       l=remove(l)
+       print("Vehicle removed successully")
+   if(do==4):
+       search(l)
+   if(do==5):
+       l=reallocate(l)
+       
